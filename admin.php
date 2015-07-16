ad4d923f9f4e380f0c5b58316b47d93d2179ea4f
aseds <s.sami.ghayeni@gmail.com>
2015-07-16 17:17:09 +0430

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,15 @@
+<!DOCTYPE>
+  <html>
+    <head></head>
+    <body>
+		<a href="/">homepage</a> <br><br>
+		<h1>Admin</h1>
+
+		<form action="pages/upload.php" method="post" enctype="multipart/form-data">
+		  Select image to upload:<br>
+		  <input type="file" name="photo" id="photo"><br>
+		  <input type="submit" value="Upload Image" name="submit">
+		</form>
+
+  </body>
+
